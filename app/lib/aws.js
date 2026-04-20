b269d8862563f183b86545e08dd47963e7535f52
Jesus Arriaza <TU_EMAIL_DE_GITHUB>
2026-04-20 16:38:53 -0600

    fix: usar REGION en lugar de AWS_REGION para Amplify

--- app/lib/aws.js
+++ app/lib/aws.js
@@ -2,7 +2,7 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
 const client = new DynamoDBClient({
-  region: process.env.AWS_REGION || 'us-east-1',
+  region: process.env.REGION || 'us-east-1',
   credentials: {
     accessKeyId: process.env.ACCESS_KEY || '',
     secretAccessKey: process.env.SECRET_KEY || '',
